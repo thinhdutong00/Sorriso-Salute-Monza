154f4a2591921d3fbfb47383a1dba35cbde85292
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-06-19 09:30:40 +0200

    Add Astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://sorrisoesalutemonza.it",
+  integrations: [sitemap()],
   build: {
     inlineStylesheets: "always",
   },
